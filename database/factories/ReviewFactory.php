05079e952320f627e48753a1662eab5c5fa3825b
Cedric Den Drijver <c-d-d@hotmail.fr>
2024-05-14 23:29:23 +0200

    first commit

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => null,
+            'review' => $this->faker->paragraphs(3, true),
+            'rating' => $this->faker->numberBetween(1, 5),
+            'created_at' => $this->faker->dateTimeBetween('-2 years'),
+            'updated_at'=> function(array $attributes) {
+                return $this->faker->dateTimeBetween($attributes['created_at']);
+            },
+        ];
+    }
+
+    public function good(): static {
+        return $this->state(function(array $attributes) {
+            return [
+                'rating' => $this->faker->numberBetween(4, 5),
+            ];
+        });
+    }
+
+    public function bad(): static {
+        return $this->state(function(array $attributes) {
+            return [
+                'rating' => $this->faker->numberBetween(1, 2),
+            ];
+        });
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use App\Models\Review;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(1)->myself()->create();
+        User::factory(10)->create();
+
+        Book::factory(20)->create()->each(function ($book) {
+            $count = random_int(5, 20);
+
+            Review::factory($count)->good()->for($book)->create();
+        });
+        Book::factory(20)->create()->each(function ($book) {
+            $count = random_int(5, 20);
+
+            Review::factory($count)->bad()->for($book)->create();
+        });
+        Book::factory(60)->create()->each(function ($book) {
+            $count = random_int(5, 20);
+
+            Review::factory($count)->for($book)->create();
+        });
+    }
+}
